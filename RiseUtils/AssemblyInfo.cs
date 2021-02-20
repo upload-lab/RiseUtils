95fba12cac176e5f9c40cc8505e9305d552d940a
upload <73221373+upload-lab@users.noreply.github.com>
2021-02-20 20:57:11 +0300

    Add files via upload

---
+++ RiseUtils/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RiseUtils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RiseUtils")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5f90ea20-5881-4969-965d-0b8a58aa3639")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ RiseUtils/RiseUtils.cs
@@ -0,0 +1,75 @@
+﻿
+using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace RiseUtils
+{
+  internal class RiseUtils
+  {
+    public static bool isValidDLLs(int id)
+    {
+      Process processById = Process.GetProcessById(id);
+      if (processById == null)
+        return true;
+      bool flag = false;
+      try
+      {
+        string lower1 = Environment.GetFolderPath(Environment.SpecialFolder.Desktop).ToLower();
+        foreach (ProcessModule module in (ReadOnlyCollectionBase) processById.Modules)
+        {
+          try
+          {
+            string lower2 = module.FileName.ToLower();
+            if (lower2.StartsWith(lower1))
+            {
+              flag = true;
+              Console.WriteLine("Not allowed #3: " + module.FileName);
+            }
+            string lower3 = module.ModuleName.ToLower();
+            if (lower3.Contains("hack") || lower3.Contains("speed") || (lower3.Contains("cheat") || lower3.Contains("click")))
+            {
+              flag = true;
+              Console.WriteLine("Not allowed #1: " + module.FileName);
+            }
+            if (!lower3.Equals("javaw.exe"))
+            {
+              if (!lower3.Equals("java.exe"))
+              {
+                if (lower2.Contains(".exe"))
+                {
+                  flag = true;
+                  Console.WriteLine("Not allowed #2: " + module.FileName);
+                }
+              }
+            }
+          }
+          catch (Exception ex)
+          {
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        return true;
+      }
+      return !flag;
+    }
+
+    public static bool isCheatEngineRunning()
+    {
+      foreach (Process process in Process.GetProcesses())
+      {
+        try
+        {
+          if (process.ProcessName.ToLower().Contains("cheatengine"))
+            return true;
+        }
+        catch (Exception ex)
+        {
+        }
+      }
+      return false;
+    }
+  }
+}
